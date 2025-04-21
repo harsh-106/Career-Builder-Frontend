df6493f7404acdfb788977ea60b69c7f9e037b3d
Harsh <ishaankatariya0581@gmail.com>
2025-04-21 19:37:55 +0530

    old

--- src/hooks/useGetScheduledInterviews.js
+++ src/hooks/useGetScheduledInterviews.js
@@ -10,10 +10,7 @@ const useGetScheduledInterviews = (userId) => {
     const fetchInterviews = async () => {
       try {
        
-        const res = await axios.get(`https://career-builder-backend.onrender.com/api/v1/interviews/user/${userId}`, {
-          withCredentials: true,
-        });
-        
+        const res = await axios.get(`/api/v1/interviews/user/${userId}`);
 
 
         console.log("📦 API Response:", res.data); 
